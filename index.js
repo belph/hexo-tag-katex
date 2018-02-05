ec89956832e448c71c9d42fc85bfb60949814bc1
Sepmein <sepmein@gmail.com>
2018-02-05 15:36:08 +0800

    Update index.js
    update katex version

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ hexo.extend.filter.register('after_post_render', function(data){
 	data.content = util.htmlTag('link',{
 		rel: 'stylesheet',
 		type: 'text/css',
-		href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css'
+		href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0-beta1/katex.min.css'
 	}) + data.content;
 
 	return data;
